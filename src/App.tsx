a9a90994d5b6e30fba12470ef01672429541e30d
aymenbend08-blip <aymenbend08@gmail.com>
2026-09-04 23:54:12 +0100

    Enable bilingual language provider

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ import Suma from "./pages/Suma";
 import Debrah from "./pages/Debrah";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import { I18nProvider } from "./i18n";
 
 export default function App() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/suma" element={<Suma />} />
-          <Route path="/products/debrah" element={<Debrah />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <I18nProvider>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/suma" element={<Suma />} />
+            <Route path="/products/debrah" element={<Debrah />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </I18nProvider>
   );
 }
