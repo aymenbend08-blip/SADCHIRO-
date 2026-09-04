742f18af5f6087df38e3c7017a155c1418fa21a3
aymenbend08-blip <aymenbend08@gmail.com>
2026-09-05 00:13:21 +0100

    Add legal pages and complete production routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,7 @@ import Suma from "./pages/Suma";
 import Debrah from "./pages/Debrah";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
 import { I18nProvider } from "./i18n";
-
-export default function App() {
-  return (
-    <I18nProvider>
-      <div className="flex min-h-screen flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/suma" element={<Suma />} />
-            <Route path="/products/debrah" element={<Debrah />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </I18nProvider>
-  );
-}
+export default function App(){return <I18nProvider><div className="flex min-h-screen flex-col"><Navbar/><main className="flex-1"><Routes><Route path="/" element={<Home/>}/><Route path="/products" element={<Products/>}/><Route path="/products/suma" element={<Suma/>}/><Route path="/products/debrah" element={<Debrah/>}/><Route path="/about" element={<About/>}/><Route path="/contact" element={<Contact/>}/><Route path="/privacy" element={<Privacy/>}/><Route path="/terms" element={<Terms/>}/></Routes></main><Footer/></div></I18nProvider>}
